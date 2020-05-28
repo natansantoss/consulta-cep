2289d165145b3a0aa1f64b1750b3647da7104e3f
Natan Santos <natansantos888@hotmail.com>
2020-05-27 22:41:41 -0300

    Criando a HTMl e CSS

--- src/page/Cep.js
+++ src/page/Cep.js
@@ -5,6 +5,28 @@ import './style.css'
 export default function Cep() {
 
     return(
-        <h1> HelloWorld </h1>
+        <div>
+            <div className="header">
+                <p className="author">By: <a href="https://github.com/natansantoss" target="_blank" rel="noopener noreferrer">Natan.Santos</a></p>
+            </div>
+
+            <div className="page">
+                <div className="valor">
+                    <p>Insira o número do CEP no campo abaixo</p>
+                    <input type="number" placeholder="00000-000" />
+                    <button>Pesquisar</button>
+                </div>
+
+                <div className="end">
+                    <p className="info">Endereço correspondente ao CEP: 00000-000</p>
+
+                    <p>Logradouro: aasdasdasdasdasdasdasd</p>
+                    <p>Bairro: aasdasdasdasdasdasdasd</p>
+                    <p>Cidade: aasdasdasdasdasdasdasd</p>
+                    <p>UF: </p>
+                
+                </div>
+            </div>
+        </div>
     )
 }
